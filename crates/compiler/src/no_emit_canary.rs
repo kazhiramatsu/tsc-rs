7a8b04959279fa668579e95d74b084f3198e0039
kazhiramatsu <kazutakehiramatsu@gmail.com>
2026-08-08 05:24:00 +0900

    h1 0b: place canary tests under unit owner

--- crates/compiler/src/no_emit_canary.rs
+++ crates/compiler/src/no_emit_canary.rs
@@ -112,31 +112,5 @@ impl NoEmitCanary {
 }
 
 #[cfg(test)]
-mod tests {
-    use std::mem::size_of;
-    use std::panic::{catch_unwind, AssertUnwindSafe};
-
-    use super::{NoEmitActivityCounters, NoEmitCanary};
-
-    macro_rules! assert_forbidden {
-        ($method:ident) => {{
-            let mut canary = NoEmitCanary::new();
-            assert!(catch_unwind(AssertUnwindSafe(|| canary.$method())).is_err());
-        }};
-    }
-
-    #[test]
-    fn proof_is_zero_sized_and_every_frozen_activity_panics() {
-        assert_eq!(size_of::<NoEmitActivityCounters>(), 0);
-        assert!(NoEmitActivityCounters.all_zero());
-
-        assert_forbidden!(construct_emit_resolver);
-        assert_forbidden!(initialize_transformers);
-        assert_forbidden!(construct_transform_context);
-        assert_forbidden!(allocate_emit_side_table);
-        assert_forbidden!(construct_printer_writer);
-        assert_forbidden!(construct_output_plan);
-        assert_forbidden!(create_emit_artifact);
-        assert_forbidden!(write_output_sink);
-    }
-}
+#[path = "../tests/unit/no_emit_canary/tests.rs"]
+mod tests;
